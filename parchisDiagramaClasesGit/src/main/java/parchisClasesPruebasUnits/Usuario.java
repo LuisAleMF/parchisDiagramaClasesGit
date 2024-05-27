b0d4aecb83113725cb8433bf2bcdee0a5e605b57
DAM114 <DAM114@B0803.iesataulfo.local>
2024-05-27 19:50:06 +0200

    Fase 5 - Creacion de la clase Usuario, le asignamos sus atributos y constructor

---
+++ parchisDiagramaClasesGit/src/main/java/parchisClasesPruebasUnits/Usuario.java
@@ -0,0 +1,16 @@
+package parchisClasesPruebasUnits;
+
+public class Usuario {
+	
+	private String username;
+	private String password;
+	
+	
+	public Usuario( String user, String pass ) {
+		
+		username=user;
+		password=pass;
+		
+	}
+
+}
